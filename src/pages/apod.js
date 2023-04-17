b8020852a0c6f75bcbf99726784cadd8886122d9
Paul Stearns <celestialcoyote@gmail.com>
2023-04-16 17:22:24 -0700

    APOD and NASA key still not working

--- src/pages/apod.js
+++ src/pages/apod.js
@@ -2,33 +2,34 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
-const apiKey = process.env.NASA_API_KEY;
+//const apiKey = process.env.NASA_API_KEY;
 
 export default function Apod() {
-	const [data, setData] = useState(null);
-	const [isLoading, setLoading] = useState(false);
+	const [photoData, setPhotoData] = useState(null);
+	//const [isLoading, setLoading] = useState(false);
 
 
-	// useEffect(() => {
-	// 	axios.get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`)
-    //     //axios.get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=2021-01-04`)
-    //         .then(res => {
-    //             setData(res.data)
-    //             console.log(res.data);
-    //             console.log(res.data.media_type);
-    //         });
-    // }, []);
+	const res = axios.get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`)
+		.then(res => {
+			setPhotoData(res.data);
+			console.log(res.data);
+			//console.log(res.data.media_type);
+		});
 
-	if (isLoading) return <p>Loading...</p>
-	if (!data) return <p>No photo data</p>
-	if (apiKey === undefined) return <p>no api key</p>
+	//if (isLoading) return <p>Loading...</p>
+	//if (!photoData) return <p>No photo data</p>
 
 	return (
 		<div className=''>
 			Astronomy Photo of the Day
-			{/* <h1>The value of customKey is: {process.env.NASA_API_KEY}</h1> */}
-			{/* <p>{data}</p> */}
-			<p>{process.env.TEST}</p>
+
+			<div>
+				<p>{photoData.title}</p>
+				<p>{photoData.date}</p>
+				<p>{photoData.explanation}</p>
+				<p>Image Credit & Copyright: {photoData.copyright}</p>
+			</div>
+
 		</div>
 	);
 };
