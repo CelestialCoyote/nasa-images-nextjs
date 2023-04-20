ebe7702cab6174caac604e2e3bd9fc834ac9191e
Paul Stearns <celestialcoyote@gmail.com>
2023-04-19 19:41:01 -0700

    more apod styling

--- src/pages/apod.js
+++ src/pages/apod.js
@@ -15,64 +15,63 @@ export default function Apod({ apodData }) {
 			<h1 className='text-3xl text-center mt-4 mb-6'>Astronomy Photo of the Day</h1>
 
 			<div className=''>
-
-				<div className='grid lg:grid-cols-2 lg:gap-4'>
-					{photoData?.media_type === 'video' ? (
-						<div className='relative h-[50vh] mb-4 lg:mb-0'>
-							<iframe
-								src={photoData?.url}
-								title={photoData?.title}
-								width='560'
-								height='349'
-								className='absolute top-0 left-0 w-full rounded-t-lg lg:rounded-lg'
-								frameBorder='0'
-								allowFullScreen
+				{photoData?.media_type === 'video' ? (
+					<div className=''>
+						<iframe
+							src={photoData?.url}
+							title={photoData?.title}
+							width='560'
+							height='349'
+							className='absolute top-0 left-0 w-full rounded-t-lg lg:rounded-lg'
+							frameBorder='0'
+							allowFullScreen
+						/>
+					</div>
+				) : (
+					<div className='flex justify-center'>
+						<a
+							href={photoData.hdurl}
+							className='rounded-lg lg:hidden focus:ring-0 focus:ring-offset-0 focus:ring-transparent'
+						>
+							<Image
+								src={photoData.url}
+								alt={photoData.title}
+								// className='object-contain object-center rounded-lg lg:object-top'
+								height={200}
+								width={200}
+								//fill={true}
+								// priority={true}
+								// loading='eager'
+								// quality={100}
 							/>
-						</div>
-					) : (
-						<div className='relative h-[50vh] rounded-lg mb-4 lg:mb-0 lg:h-auto focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-0 focus-within:ring-indigo-400'>
+						</a>
+						<div className='hidden lg:sticky lg:block lg:top-10'>
 							<a
-								href={photoData?.hdurl}
-								className='rounded-lg lg:hidden focus:ring-0 focus:ring-offset-0 focus:ring-transparent'
+								href={photoData.hdurl}
+								className='rounded-lg focus:ring-0 focus:ring-offset-0 focus:ring-transparent'
 							>
-								<Image
-									src={photoData?.url}
-									alt={photoData?.title}
-									className='object-contain object-center rounded-lg lg:object-top'
-									layout='fill'
-									priority={true}
+								<img
+									src={photoData.url}
+									alt={photoData.title}
 									loading='eager'
-									quality={100}
+									className='rounded-lg'
 								/>
 							</a>
-							<div className='hidden lg:sticky lg:block lg:top-10'>
-								<a
-									href={photoData?.hdurl}
-									className='rounded-lg focus:ring-0 focus:ring-offset-0 focus:ring-transparent'
-								>
-									<img
-										src={photoData?.url}
-										alt={photoData?.title}
-										loading='eager'
-										className='rounded-lg'
-									/>
-								</a>
-							</div>
 						</div>
-					)}
-
-					<div className='p-4 bg-black rounded-b-lg sm:p-8 lg:rounded-lg'>
-						<p className='pt-2 text-primary lg:pt-0'>{photoData?.date}</p>
-						<h1 className='py-2 text-4xl font-medium text-gray-200 glow'>
-							{photoData?.title}
-						</h1>
-						{photoData?.copyright ? (
-							<h2 className='text-lg text-gray-400'>{`Credit: ${photoData.copyright}`}</h2>
-						) : null}
-						<p className='pt-2 text-xl leading-relaxed text-gray-300'>
-							{photoData?.explanation}
-						</p>
 					</div>
+				)}
+
+				<div className='mt-6 p-8'>
+					<p className='pt-2 text-primary lg:pt-0'>{photoData.date}</p>
+					<h1 className='py-2 text-4xl font-medium text-gray-200 glow'>
+						{photoData.title}
+					</h1>
+					{photoData.copyright ? (
+						<h2 className='text-lg text-gray-400'>{`Credit: ${photoData.copyright}`}</h2>
+					) : null}
+					<p className='pt-2 text-xl leading-relaxed text-gray-300'>
+						{photoData.explanation}
+					</p>
 				</div>
 			</div>
 
