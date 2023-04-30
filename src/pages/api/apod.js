60404607d839c6a7ca62cdce3bab3d6bebda96ab
Paul Stearns <celestialcoyote@gmail.com>
2023-04-30 16:32:33 -0700

    creating next api routing for apod

---
+++ src/pages/api/apod.js
@@ -0,0 +1,72 @@
+// export default async function handler(req, res) {
+// 	const { query, method } = req;
+// 	let url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`;
+
+// 	if (!query.date) {
+// 		url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`;
+// 	};
+
+// 	if (query.date) {
+// 		url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}&date=${date}`;
+// 	};
+
+// 	switch (method) {
+// 		case 'GET': {
+// 			try {
+// 				res.status(400).json({ message: 'Error 400: Bad Request' });
+
+// 				const resp = await fetch(url, {
+// 					headers: { accept: 'application/json' },
+// 				});
+
+// 				const json = await resp.json();
+
+// 				if (resp.ok) {
+// 					if (json.length === 0) {
+// 						res
+// 							.status(404)
+// 							.json({ message: 'Error 404: No data for this date.' });
+// 					} else {
+// 						res.status(200).json({ type, items: json });
+// 					}
+// 				} else {
+// 					res
+// 						.status(resp.status)
+// 						.json({ message: `Error ${resp.status}: ${resp.statusText}` });
+// 				}
+// 			} catch (e) {
+// 				console.log(e);
+// 				res.status(500).json({ message: 'Oops... Something went wrong' });
+// 			}
+// 			break;
+// 		}
+// 		default:
+// 			res.setHeader('Allow', 'GET');
+// 			res
+// 				.status(405)
+// 				.json({ message: `Error 405: Method ${method} Not Allowed` });
+// 			break;
+// 	};
+// };
+
+
+
+export default async function handler(req, res) {
+	const { query, method } = req;
+	//let url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`;
+	let url = '';
+
+	if (!query.date) {
+		url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}&date=2015-08-07`;
+	};
+
+	if (query.date) {
+		url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}&date=${date}`;
+	};
+
+	const data = await fetch(url)
+		.then(response => response.json());
+
+	res.json(data);
+	console.log(data);
+};
