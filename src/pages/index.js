968a8a013faddc897e016cd4f7b68080f2cfb83e
Paul Stearns <celestialcoyote@gmail.com>
2023-08-06 20:15:46 -0700

    added css to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,13 +11,36 @@ export default function Home() {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
-			<div className='flex flex-col items-center'>
-				<h1 className='text-3xl mb-8'>NASA API</h1>
-				<Link className='mb-6' href="/apod">Astronomy Photo of the Day</Link>
-				<Link className='mb-6' href="/image-video-library">Image and Video Library</Link>
+			<div className='flex flex-col items-center mt-10'>
+				<h1 className='text-6xl mb-10'>NASA API</h1>
+
+				<div className="flex flex-col items-center mb-8">
+					<Link
+						className="text-2xl mb-6 text-cyan-500"
+						href="/apod"
+					>
+							Astronomy Photo of the Day
+					</Link>
+
+					<div className="w-2/3">
+						<p>
+							One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of the most popular websites across all federal agencies.
+						</p>
+					</div>
+				</div>
+
+				<div className="flex flex-col">
+					<Link
+						className="text-2xl mb-6 text-cyan-500"
+						href="/image-video-library"
+					>
+						Image and Video Library
+					</Link>
+				</div>
+
 			</div>
 
-			
+
 		</div>
 	);
 };
